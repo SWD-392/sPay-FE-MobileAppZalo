56e76915721a3f29e4148af741e8e21bb85c671b
Ho Chi Trung <trunghcw@gmail.com>
2024-03-23 01:19:51 +0700

    final lan 3

--- src/service/order.ts
+++ src/service/order.ts
@@ -12,11 +12,11 @@ export const creatOrder = async (body:any) => {
     const response = await axios.post(
         `${BASE_URL}${QUERY_PROMOTION}`,
         body, // Add the body here
-        // {
-        //   headers: {
-        //     Authorization: `Bearer ${token}`, // Add the token to the headers
-        //   },
-        // }
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // Add the token to the headers
+          },
+        }
       );
       return response;
   } catch (error) {
